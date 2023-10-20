9bfc9c0e5b8e379cbb5be3e1567d72e72dba4a82
Debmalya Kuila <debmalyakuila911@gmail.com>
2023-10-21 02:51:03 +0530

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,29 @@
 import { Typography } from "antd";
 import React from "react";
+import {
+  FacebookOutlined,
+  InstagramOutlined,
+  YoutubeOutlined,
+  TwitterOutlined,
+} from "@ant-design/icons";
 
 const Footer = () => {
   return (
-    <div className="px-8 py-5 bg-slate-600 flex flex-col justify-center items-center">
-      <Typography.Title level={5} style={{ color: "white" }}>
-        Copyright &copy; 2023 ,owned by Debmalya Kuila
-      </Typography.Title>
-      <div className=" w-full flex flex-row justify-between">
-        <div className="flex flex-col">
-          <Typography.Link href="#" target={"_blank"}>
-            Privacy and Policies
-          </Typography.Link>
-          <Typography.Link href="#" target={"_blank"}>
-            Terms & Conditions
-          </Typography.Link>
-          <Typography.Link href="#" target={"_blank"}>
-            +91-xxxxxxxxxx
-          </Typography.Link>
-          <Typography.Link href="#" target={"_blank"}>
-            abc@gmail.com
-          </Typography.Link>
-        </div>
-        <div className="flex flex-col">
-          <Typography.Link href="#" target={"_blank"}>facebook.com/Shopzee</Typography.Link>
-          <Typography.Link href="#" target={"_blank"}>instagram.com/Shopzee</Typography.Link>
-        </div>
+    <div className=" bg-slate-600 flex flex-col justify-center items-center py-4">
+      <div
+        className="w-4/12 flex flex-row justify-around items-center mb-4"
+        style={{ fontSize: 26, color: "white" }}
+      >
+        <FacebookOutlined />
+        <InstagramOutlined />
+        <YoutubeOutlined />
+        <TwitterOutlined />
       </div>
+      <Typography.Title level={5}>
+        <Typography.Link style={{ color: "white" }}>
+          Copyright &copy; 2023,owned by Debmalya Kuila
+        </Typography.Link>
+      </Typography.Title>
     </div>
   );
 };
